08533805d5f651bd6862dbaa28dd806105c571d0
Vick Kumar <vick.kumar44@gmail.com>
2025-11-27 08:33:51 -0500

    Revert "pulling data"

--- app/chatbot-embed/page.tsx
+++ app/chatbot-embed/page.tsx
@@ -1,24 +1,9 @@
-"use client";
-
-import FloatingChatbot from "../components/FloatingChatbot";
-import { useSearchParams } from "next/navigation";
-import { Suspense } from "react";
-
-function ChatbotEmbedContent() {
-  const searchParams = useSearchParams();
-  const shop = searchParams.get("shop") || "unknown";
-
-  return (
-    <div>
-      <FloatingChatbot shopDomain={shop} />
-    </div>
-  );
-}
+import FloatingChatbot from '../components/FloatingChatbot';
 
 export default function ChatbotEmbed() {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <ChatbotEmbedContent />
-    </Suspense>
+    <div>
+      <FloatingChatbot />
+    </div>
   );
 }
